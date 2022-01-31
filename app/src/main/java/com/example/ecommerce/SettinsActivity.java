4ccf217d24cb790a85a93bcf01b2727c8005632d
Lazar Elvis <lazarelvis15@gmail.com>
2022-01-31 21:50:06 +0200

    View interface settings update profile. (tutorial #17)

---
+++ app/src/main/java/com/example/ecommerce/SettinsActivity.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettinsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settins);
+    }
+}
