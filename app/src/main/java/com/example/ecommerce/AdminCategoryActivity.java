ea7593b2bb0a5417b7b13a79709c8c0f850a8466
Lazar Elvis <lazarelvis15@gmail.com>
2022-02-02 23:15:49 +0200

    Admin section add LogOut button (tutorial #29)

--- app/src/main/java/com/example/ecommerce/AdminCategoryActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminCategoryActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class AdminCategoryActivity extends AppCompatActivity {
@@ -13,12 +15,35 @@ public class AdminCategoryActivity extends AppCompatActivity {
     private ImageView glasses, hatCaps, walletsBagsPurses, shoes;
     private ImageView headphones, laptops, watches, mobilePhones;
 
+    private Button LogoutBtn, CheckOrdersBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_category);
 
         initializeVariables();
+
+        LogoutBtn = (Button) findViewById(R.id.admin_logout_btn);
+        CheckOrdersBtn = (Button) findViewById(R.id.check_orders_btn);
+
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+        CheckOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminNewOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         tShirts.setOnClickListener(v -> {
             Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
